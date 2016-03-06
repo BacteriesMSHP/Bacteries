6b9315e72e27d5d2cc9e4139d249284d396597af
Artom-Kozincev <s17b2_kozincev@179.ru>
2016-03-06 22:40:03 +0300

    Added header file for Interpreter class
    For now contains only member Bactery* world[][], which contains
    bacteries and it's getter. Is setter needed here?

---
+++ interpreter.hpp
@@ -0,0 +1,16 @@
+#ifndef INTERPRETER_HPP
+#define INTERPRETER_HPP
+
+#define WORLD_SIZE 20
+
+class Bactery;
+
+class Interpreter {
+private:
+	Bactery* world[WORLD_SIZE][WORLD_SIZE];
+public:
+	Interpreter();
+	Bactery*[][] getWorld();
+};
+
+#endif // INTERPRETER_HPP
