933266037e509456c49f1d97fec6619a911c3164
Artom-Kozincev <s17b2_kozincev@179.ru>
2016-04-17 05:48:59 +0300

    Fixed Interpreter header

--- interpreter.hpp
+++ interpreter.hpp
@@ -1,16 +1,23 @@
 #ifndef INTERPRETER_HPP
 #define INTERPRETER_HPP
+#include <vector>
+#include <string>
+
+using namespace std;
 
 #define WORLD_SIZE 20
 
 class Bactery;
+class League;
 
 class Interpreter {
 private:
     Bactery* world[WORLD_SIZE][WORLD_SIZE];
+	vector <League> leagues;
 public:
-    Interpreter();
-    Bactery*[][] &getWorld();
+    Interpreter(vector <string>);
+    Bactery *getWorldItem(int, int);
+	Bactery *setWorldItem(int, int, Bactery*);
 };
 
 #endif // INTERPRETER_HPP
